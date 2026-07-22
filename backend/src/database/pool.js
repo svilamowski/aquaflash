a03e63fb684e4caebbaaff7e8103dd9e0047051e
Nicolas <nsimone@fi.uba.ar>
2026-07-22 14:17:06 -0300

    cambios en pool.js para poder hacer el deploy en vercel

--- backend/src/database/pool.js
+++ backend/src/database/pool.js
@@ -12,12 +12,15 @@ for (const key of required) {
     }
 }
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
     host: process.env.DATABASE_HOST || 'localhost',
     port: Number(process.env.DATABASE_PORT || 5432),
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
